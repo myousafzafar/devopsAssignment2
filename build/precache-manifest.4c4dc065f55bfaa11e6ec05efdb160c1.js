7d83f01c48efd2aae15e67a80fd189c3101aa45a
Muhammad Yousaf Zafar <83083964+myousafzafar@users.noreply.github.com>
2022-11-24 21:52:04 +0500

    Add files via upload

---
+++ build/precache-manifest.4c4dc065f55bfaa11e6ec05efdb160c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b60b35162b89cd9ff23f7de71a7f2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d01aa3f536e87a9b4b2f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2ccacbf25b427b2b3a63",
+    "url": "/static/css/main.14ff7981.chunk.css"
+  },
+  {
+    "revision": "d01aa3f536e87a9b4b2f",
+    "url": "/static/js/2.67710b93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67710b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ccacbf25b427b2b3a63",
+    "url": "/static/js/main.1017a307.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
